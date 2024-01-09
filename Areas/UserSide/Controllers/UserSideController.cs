6ac120fa989df62db8ef170c7dc99742fe70621c
pratiktank11 <21010101189@darshan.ac.in>
2024-01-09 19:27:29 +0530

    changes in view of userside area

---
+++ Areas/UserSide/Controllers/UserSideController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Project_Final.Areas.UserSide.Controllers
+{
+    [Area("UserSide")]
+    [Route("UserSide/[controller]/[action]")]
+    public class UserSideController : Controller
+    {
+       public IActionResult Service()
+        {
+			return View();
+		}
+
+		public IActionResult NewCars()
+        {
+            return View();
+        }
+
+		public IActionResult Home()
+		{
+			return View();
+		}
+
+		public IActionResult Brands()
+		{
+			return View();
+		}
+
+		public IActionResult Contact()
+		{
+			return View();
+		}
+
+		public IActionResult FeaturedCars()
+		{
+			return View();
+		}
+	}
+}
